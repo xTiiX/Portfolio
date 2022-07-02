218cd6d1374747d19ca3742f68e14eabd9f8c62b
xTiiX <anordmann.pro@gmail.com>
2022-07-02 09:48:29 +0100

    MOD : Test left scroll -> Need to lock down scroll

---
+++ FRONT/nova/resources/views/test.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link rel="stylesheet" href="{{ mix('css/anims.css') }}">
+    </head>
+    <body>
+        <div class="container">
+            <section>
+                <br><br><br>
+                <p>Test de Spheres :</p><br><br><br>
+                <div class="content">
+                    <svg class="animate-slow-down" width="119" height="119" viewBox="0 0 119 119" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="59.5" cy="59.5" r="59.5" fill="#F97316"/>
+                    </svg>
+                    <svg class="animate-slow-down-2" width="119" height="119" viewBox="0 0 119 119" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="59.5" cy="59.5" r="59.5" fill="#F97316"/>
+                    </svg>
+                    <svg class="animate-slow-down-3" width="119" height="119" viewBox="0 0 119 119" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="59.5" cy="59.5" r="59.5" fill="#F97316"/>
+                    </svg>
+                    <svg class="animate-slow-up-3" width="119" height="119" viewBox="0 0 119 119" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="59.5" cy="59.5" r="59.5" fill="#F97316"/>
+                    </svg>
+                    <svg class="animate-slow-up-2" width="119" height="119" viewBox="0 0 119 119" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="59.5" cy="59.5" r="59.5" fill="#F97316"/>
+                    </svg>
+                    <svg class="animate-slow-up" width="119" height="119" viewBox="0 0 119 119" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="59.5" cy="59.5" r="59.5" fill="#F97316"/>
+                    </svg>
+                    <svg class="animate-sup-2s" width="119" height="119" viewBox="0 0 119 119" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="59.5" cy="59.5" r="59.5" fill="#F97316"/>
+                    </svg>
+                    <svg class="animate-slow-up" width="219" height="219" viewBox="0 0 219 219" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="109.5" cy="109.5" r="109" fill="#F4C10A" stroke="#F4C10A"/>
+                    </svg>
+                </div>
+                <div class="content m-10"></div>
+                <div class="content">
+                    <svg class="animate-slow-up" width="119" height="119" viewBox="0 0 119 119" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="59.5" cy="59.5" r="59.5" fill="#F97316"/>
+                    </svg>
+                    <svg class="animate-sup-1s" width="119" height="119" viewBox="0 0 119 119" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="59.5" cy="59.5" r="59.5" fill="#F97316"/>
+                    </svg>
+                    <svg class="animate-sup-2s" width="119" height="119" viewBox="0 0 119 119" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="59.5" cy="59.5" r="59.5" fill="#F97316"/>
+                    </svg>
+                    <svg class="animate-sup-3s" width="119" height="119" viewBox="0 0 119 119" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="59.5" cy="59.5" r="59.5" fill="#F97316"/>
+                    </svg>
+                    <svg class="animate-sup-4s" width="119" height="119" viewBox="0 0 119 119" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="59.5" cy="59.5" r="59.5" fill="#F97316"/>
+                    </svg>
+                    <svg class="animate-sup-5s" width="119" height="119" viewBox="0 0 119 119" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="59.5" cy="59.5" r="59.5" fill="#F97316"/>
+                    </svg>
+                    <svg class="animate-sup-6s" width="119" height="119" viewBox="0 0 119 119" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="59.5" cy="59.5" r="59.5" fill="#F97316"/>
+                    </svg>
+                    <svg class="animate-sup-7s" width="119" height="119" viewBox="0 0 119 119" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="59.5" cy="59.5" r="59.5" fill="#F97316"/>
+                    </svg>
+                    <svg class="animate-sup-8s" width="119" height="119" viewBox="0 0 119 119" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="59.5" cy="59.5" r="59.5" fill="#F97316"/>
+                    </svg>
+                </div>
+            </section>
+        </div>
+    </body>
+</html>
